25de6813a09c1a1c9de24d42b78874a87c8ec4c6
Sun Galan <sunrise.galan@gmail>
2021-10-31 03:14:54 +0800

    debug false

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'tqsi6w83)272sedojbq(s#$&bisvbpd)z1s!pz!q2q(+a8bh49'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
